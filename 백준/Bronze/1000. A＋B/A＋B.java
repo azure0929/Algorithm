9ef9002b7ade27c4eff61746d0f94874ed7c5442
JunYong <128226527+azure0929@users.noreply.github.com>
2025-04-11 22:23:28 +0900

    [Bronze V] Title: A+B, Time: 176 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt(); // 첫 번째 정수 입력
+        int B = sc.nextInt(); // 두 번째 정수 입력
+        
+        System.out.println(A + B); // 출력
+    }
+}

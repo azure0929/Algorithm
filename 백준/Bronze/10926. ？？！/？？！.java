283f69e2718d413383e74ee7cc6b9c8990d9b2f1
JunYong <128226527+azure0929@users.noreply.github.com>
2025-04-13 20:03:31 +0900

    [Bronze V] Title: ??!, Time: 176 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String id = sc.nextLine(); // 사용자 id 입력
+        System.out.println(id + "??!");
+    }
+}

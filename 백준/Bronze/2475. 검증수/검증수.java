134c7703b408c6c0222d27f201fcb39475cd4807
JunYong <128226527+azure0929@users.noreply.github.com>
2025-05-15 23:04:18 +0900

    [Bronze V] Title: 검증수, Time: 172 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int result = 0;
+        for (int i = 0; i < 5; i++) {
+            int num = sc.nextInt();
+            result += (num * num);
+        }
+        
+        System.out.println(result % 10);
+        
+        sc.close();
+    }
+}
